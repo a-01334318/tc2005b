11e35d0b6bfc82c52ec7a67f9a067c6f8134b764
Erik <a01334318@tec.mx>
2023-03-12 16:54:14 -0600

    instalar ejs, agregar public y views

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.urlencoded({extended: false}));
 
