060388459a20a4295df23663f4820202397db776
Erik <a01334318@tec.mx>
2023-03-13 16:43:44 -0600

    agregar express-session

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const session = require('express-session');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
+app.use(session({
+    secret: 'mi string secreto que debe ser un string aleatorio muy largo, no como éste', 
+    resave: false, //La sesión no se guardará en cada petición, sino sólo se guardará si algo cambió 
+    saveUninitialized: false, //Asegura que no se guarde una sesión para una petición que no lo necesita
+}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
