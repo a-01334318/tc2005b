8b5a0ac62166954b36c46d94ece07b297126831e
Erik <a01334318@tec.mx>
2023-03-15 18:19:37 -0600

    autentificar usuarios

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,4 +1,5 @@
 const User = require('../models/users.model');
+const bcrypt = require('bcryptjs');
 
 exports.get_signup = (request, response, next) => {
     response.render('signup');
@@ -39,7 +40,18 @@ exports.post_login = (request, response, next) => {
     User.fetchOne(request.body.inputUsername)
     .then(([rows, fieldData]) => {
         if (rows.length > 0) {
-
+            bcrypt.compare(request.body.iputPassword, rows[0].password)
+            .then((doMatch) => {
+                if (doMatch) {
+                    request.session.isLoggedIn = true;
+                    request.session.username = rows[0].username;
+                    response.redirect('/list');
+                } else {
+                    request.session.alert = 'El usuario y/o contraseña no coinciden';
+                    response.redirect('/login');
+                }
+            })
+            .catch((error) => {console.log(error)});
         } else {
             request.session.alert = 'El usuario y/o contraseña no coinciden';
             response.redirect('/login');
@@ -47,8 +59,6 @@ exports.post_login = (request, response, next) => {
     })
     .catch((error) => {console.log(error)});
 }
-
-
 
 exports.logout = (request, response, next) => {
     request.session.destroy(() => {
