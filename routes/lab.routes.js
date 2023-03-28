f2c0f03accbd5ae9fa9e9b03748b9d48452c1fed
Erik <a01334318@tec.mx>
2023-03-27 19:25:47 -0600

    corregir generar interface a partir de privilegios

--- routes/lab.routes.js
+++ routes/lab.routes.js
@@ -5,10 +5,10 @@ const controller = require('../controllers/lab.controller')
 const hasCreate = require('../util/has-create');
 
 router.get('/', controller.get_index);
-router.get('/contact', controller.get_contact);
-router.post('/contact', controller.post_contact);
-router.get('/list', hasCreate, controller.get_list);
-router.get('/list/:id', hasCreate, controller.get_list);
+router.get('/contact', hasCreate, controller.get_contact);
+router.post('/contact', hasCreate, controller.post_contact);
+router.get('/list', controller.get_list);
+router.get('/list/:id', controller.get_list);
 router.get('/faq', controller.get_faq);
 
 module.exports = router;
